066aeba944156c886ae9dee7c96c16d474304b01
Mihail Diordiev <zalmoxisus@gmail.com>
2016-10-05 14:50:10 +0300

    Fix `hasOwnProperty` method of `undefined` exception (#44)

--- src/DiffPatcher.js
+++ src/DiffPatcher.js
@@ -2,7 +2,8 @@ import { DiffPatcher } from 'jsondiffpatch/src/diffpatcher';
 
 const diffpatcher = new DiffPatcher({
   arrays: { detectMove: false },
-  objectHash: (o, idx) => o.hasOwnProperty('id') ? o.id : '$$index:' + idx,
+  objectHash: (o, idx) =>
+    typeof o === 'object' && o.hasOwnProperty('id') ? o.id : '$$index:' + idx,
   propertyFilter: (name, context) =>
     typeof context.left[name] !== 'function' &&
     typeof context.right[name] !== 'function'

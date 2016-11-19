5c3c341aa37d726fccee4856e5399b6a98dc658d
Kuzya <alexkuz@gmail.com>
2016-11-19 16:26:33 +0300

    prevent line wrapping for item preview

--- src/tabs/getJsonTreeTheme.js
+++ src/tabs/getJsonTreeTheme.js
@@ -1,6 +1,12 @@
 export default function getJsonTreeTheme(base16Theme) {
   return {
     extend: base16Theme,
+    nestedNode: ({ style }, keyPath, nodeType, expanded) => ({
+      style: {
+        ...style,
+        'white-space': expanded ? 'inherit' : 'nowrap'
+      }
+    }),
     nestedNodeItemString: ({ style }, keyPath, nodeType, expanded) => ({
       style: {
         ...style,
